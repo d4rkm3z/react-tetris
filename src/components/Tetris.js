578ac291edfb9129b2a3da4d4007cc74ffb7a358
Kirill Sadovnikov <d4rkm3z@gmail.com>
2019-08-29 23:43:14 +0300

    feat: components stylization

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -3,20 +3,23 @@ import Display from './Display';
 import Stage from './Stage';
 import StartButton from './StartButton';
 import { createStage } from '../gameHelpers';
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()}/>
-      <aside>
-        <div>
-          <Display text="Score"/>
-          <Display text="Rows"/>
-          <Display text="Level"/>
-        </div>
-        <StartButton/>
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()}/>
+        <aside>
+          <div>
+            <Display text="Score"/>
+            <Display text="Rows"/>
+            <Display text="Level"/>
+          </div>
+          <StartButton/>
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   )
 };
 
